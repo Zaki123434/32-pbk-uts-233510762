0057c1016540265d12c87aa77b21d357e03b5d29
Zaki123434 <ahmadzaki5495@gmail.com>
2025-05-06 21:47:34 +0700

    filter data selesai, belum, semua

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 
 const tasks = ref([]);
 const newTask = ref('');
+const filter = ref('semua');
 
 const addTask = () => {
   if (newTask.value.trim() !== '') {
@@ -23,15 +24,29 @@ const toggleTask = (task) => {
   task.completed == !task.completed;
 }
 
+const filteredTasks = computed(() => {
+  if (filter.value === 'selesai') {
+    return tasks.value.filter(task => task.completed);
+  } else if (filter.value === 'belum selesai') {
+    return tasks.value.filter(task => !task.completed);
+  } else {
+    return tasks.value;
+  }
+});
+
 </script>
 
 <template>
   <div>
     <input type="text" v-model="newTask" @keyup.enter="addTask">
     <button @click="addTask">Add Task</button>
-
+    <select v-model="filter">
+      <option value="semua">Semua</option>
+      <option value="selesai">Selesai</option>
+      <option value="belum selesai">Belum Selesai</option>
+    </select>  
     <ul>
-      <li v-for="task in tasks" :key="task.id">
+      <li v-for="task in filteredTasks" :key="task.id">
         <input type="checkbox" v-model="task.completed" @change="toggleTask(task)">
         {{ task.text }}
         <button @click="removeTask(task)">Remove</button>
